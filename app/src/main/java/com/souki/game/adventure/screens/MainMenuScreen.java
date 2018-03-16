35cf693c197bac90b76ed09df3e9e829b96b5a56
Caerwent <caerwent@gmail.com>
2018-03-16 09:37:32 +0100

    add level

--- app/src/main/java/com/souki/game/adventure/screens/MainMenuScreen.java
+++ app/src/main/java/com/souki/game/adventure/screens/MainMenuScreen.java
@@ -10,20 +10,13 @@ import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.souki.game.adventure.AssetsUtility;
 import com.souki.game.adventure.MyGame;
 import com.souki.game.adventure.audio.AudioManager;
-import com.souki.game.adventure.items.Item;
-import com.souki.game.adventure.items.ItemFactory;
+import com.souki.game.adventure.persistence.LocationProfile;
 import com.souki.game.adventure.persistence.PersistenceProvider;
 import com.souki.game.adventure.persistence.Profile;
-import com.souki.game.adventure.quests.Quest;
-import com.souki.game.adventure.quests.QuestManager;
-import com.souki.game.adventure.quests.QuestTask;
-
-import java.util.ArrayList;
 
 import static com.souki.game.adventure.Settings.TARGET_HEIGHT;
 import static com.souki.game.adventure.Settings.TARGET_WIDTH;
@@ -201,7 +194,7 @@ public class MainMenuScreen implements Screen {
 
  /*CHANGE QUEST STATE*/
 
-        Quest quest = QuestManager.getInstance().getQuestFromId("quest_malo_blacksmith");
+  /*      Quest quest = QuestManager.getInstance().getQuestFromId("quest_malo_blacksmith");
         quest.setActivated(true);
         quest.setCompleted(false);
         for(QuestTask task : quest.getTasks())
@@ -209,19 +202,19 @@ public class MainMenuScreen implements Screen {
             task.setCompleted(false);
         }
         Profile.getInstance().updateQuestProfile("quest_malo_blacksmith", quest);
-
+*/
       /*
        CHANGE LOCATION*/
-/*
+
         LocationProfile locationProfile = new LocationProfile();
-        locationProfile.mMapId = "mines2";
+        locationProfile.mMapId = "mines3";
         // locationProfile.mFromMapId = aFromMap;
 
-        Profile.getInstance().setLocationProfile(locationProfile);*/
+        Profile.getInstance().setLocationProfile(locationProfile);
 
-      /*
-      CHANGE ITEMS*/
 
+/*      CHANGE ITEMS*/
+/*
         Array<Item> inventory = new Array<Item>();
         ArrayList<String> savedInventory = Profile.getInstance().getInventory();
 
@@ -235,7 +228,7 @@ public class MainMenuScreen implements Screen {
 
         Profile.getInstance().updateInventory(inventory);
 
-
+*/
        /*
        CHANGE MAP ITEM STATE*/
        /*
